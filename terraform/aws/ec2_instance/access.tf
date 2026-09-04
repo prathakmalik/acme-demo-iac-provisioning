f6e2084a64c17f1c67ed79546b4496d40b56b8ec
prathakmalik <77539912+prathakmalik@users.noreply.github.com>
2026-09-04 16:35:52 +0530

    Add IAM user login profile configuration for EC2 instance access

--- terraform/aws/ec2_instance/access.tf
+++ terraform/aws/ec2_instance/access.tf
@@ -5,6 +5,7 @@ data "aws_iam_users" "search_user" {
 }
 
 resource "aws_iam_user" "create_user" {
+  # Only create a new user, if search_user returned no results (i.e. the user does not already exist)
   count = length(data.aws_iam_users.search_user.names) == 0 ? 1 : 0
 
   name = var.requester_username
@@ -14,6 +15,24 @@ resource "aws_iam_user" "create_user" {
     Name              = var.requester_username
     Environment       = "Demo"
     Decommission-Date = var.decommission_date
+  }
+}
+
+resource "aws_iam_user_login_profile" "developer_login" {
+  # Only create a password profile if a new user was actually generated
+  count = length(data.aws_iam_users.search_user.names) == 0 ? 1 : 0
+
+  user                    = aws_iam_user.create_user[0].name
+  password_length         = 16
+  password_reset_required = true
+
+  # CRITICAL: Prevent consecutive "terraform apply" commands from fighting 
+  # with AWS over whether the user has reset their password yet!
+  lifecycle {
+    ignore_changes = [
+      password_reset_required,
+      password_length
+    ]
   }
 }
 

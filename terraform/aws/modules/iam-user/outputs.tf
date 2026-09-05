a0f960aa0148e63c895826ad9e494dbc04563f02
prathakmalik <77539912+prathakmalik@users.noreply.github.com>
2026-09-05 21:06:39 +0530

    Refactor user_login_details output to remove jsonencode for improved readability

--- terraform/aws/modules/iam-user/outputs.tf
+++ terraform/aws/modules/iam-user/outputs.tf
@@ -4,11 +4,11 @@ locals {
 
 output "user_login_details" {
     description = "AWS login details for the user"
-    value = jsonencode({
+    value = {
         user_created = local.user_created
         username = local.user_created ? aws_iam_user.create_user[0].name : tolist(data.aws_iam_users.search_user.names)[0]
         userarn = local.user_created ? aws_iam_user.create_user[0].arn : tolist(data.aws_iam_users.search_user.arns)[0]
         password = local.user_created ? aws_iam_user_login_profile.developer_login[0].password : null
-    })
+    }
     sensitive = true
 }
